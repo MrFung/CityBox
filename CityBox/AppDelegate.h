092671d293ea01773f50ea77f9dd59c7225fe4f0
MrFung <2578033436@qq.com>
2017-10-21 16:53:54 +0800

    Initial Commit

---
+++ CityBox/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CityBox
+//
+//  Created by 郭枫 on 2017/10/21.
+//  Copyright © 2017年 郭枫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ CityBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityBox
+//
+//  Created by 郭枫 on 2017/10/21.
+//  Copyright © 2017年 郭枫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
